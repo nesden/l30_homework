ef4d5b3947ac4f41a6a81e859ebf4e2a39c65de4
nes_denon <nesdenon@gmail.com>
2024-11-27 17:11:33 +0200

    homework

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class HomeWork {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 30; i++) {
+        for (int i = 0; i <= 30; i+=2) {
             System.out.println(i);
         }
 
